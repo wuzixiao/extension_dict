bcabc2b1cbdb99cedbcc47018905cdcf28592b99
wuzixiao <wuzi.xiao@gmail.com>
2014-02-05 13:25:45 +1300

    add translate with google

--- translate.js
+++ translate.js
@@ -9,7 +9,19 @@ chrome.contextMenus.create({
 	"onclick": onClickHandler
 });
 
+chrome.contextMenus.create({
+	"title": "Translate with google",
+	"type": "normal",
+	"contexts": ["selection"],
+	"onclick": onClickHandler2
+});
+
+
 function onClickHandler(info) {
 	var url = "http://www.dict.cn/" + info.selectionText;
+	chrome.tabs.create({ url: url, active: true });
+};
+function onClickHandler2(info) {
+	var url = "http://translate.google.com/#auto/zh-CN/" + info.selectionText;
 	chrome.tabs.create({ url: url, active: true });
 };
