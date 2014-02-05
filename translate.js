5068cfb4cdfcf556cad8f4ab4059c8f61413b774
wuzixiao <wuzi.xiao@gmail.com>
2014-02-05 20:56:08 +1300

    one english word will be translated with dict.cn , otherwise with google

--- translate.js
+++ translate.js
@@ -1,27 +1,21 @@
-//copyright
-//
-//
-
 chrome.contextMenus.create({
-	"title": "Translate %s with dict.cn",
+	"title": "Translate This",
 	"type": "normal",
 	"contexts": ["selection"],
 	"onclick": onClickHandler
 });
 
-chrome.contextMenus.create({
-	"title": "Translate with google",
-	"type": "normal",
-	"contexts": ["selection"],
-	"onclick": onClickHandler2
-});
-
 
 function onClickHandler(info) {
-	var url = "http://www.dict.cn/" + info.selectionText;
-	chrome.tabs.create({ url: url, active: true });
-};
-function onClickHandler2(info) {
-	var url = "http://translate.google.com/#auto/zh-CN/" + info.selectionText;
+	var str = info.selectionText;
+	var reg=/^[a-z]*$/gi;
+
+	if(reg.test(str)) { 
+		var url = "http://www.dict.cn/" + str;
+	}
+	else {
+		var url = "http://translate.google.com/#auto/zh-CN/" + str;
+	}
+
 	chrome.tabs.create({ url: url, active: true });
 };
